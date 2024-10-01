23b8dba523036cdb4a6fa1131e7e26d8e7fe1997
omGazz <omgazz89@gmail.com>
2024-10-01 09:41:40 +0200

    fix: onPress event on spectrum button

--- src/js/main/components/button.tsx
+++ src/js/main/components/button.tsx
@@ -1,26 +1,25 @@
 import { Button } from "@adobe/react-spectrum";
 
 type ButtonProps = {
-    text: string;
-    onClick: () => void;
-}
+  text: string;
+  onClick: () => void;
+};
 
 //TODO: Fix this, it's not working on press but if I use a normal button it works
+const DcButton = ({ text = "placeholder", onClick }: ButtonProps) => {
+  return (
+    <Button variant="cta" onPress={onClick}>
+      {text}
+    </Button>
+  );
+};
+
 // const DcButton = ({ text = "placeholder", onClick }: ButtonProps) => {
 //     return (
-//             <Button 
-//             variant="cta"
-//             onPress={onClick}
-//             >{text}</Button>
+//             <button
+//             onClick={onClick}
+//             >{text}</button>
 //     );
 // }
-
-const DcButton = ({ text = "placeholder", onClick }: ButtonProps) => {
-    return (
-            <button 
-            onClick={onClick}
-            >{text}</button>
-    );
-}
 
 export default DcButton;
